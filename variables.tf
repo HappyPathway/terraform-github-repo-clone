9a60e4c491382dcdd7ed51c172b4c194795650cb
Dave Arnold <dja+djaboxx@sonic.net>
2018-08-23 10:39:06 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,8 @@ variable "repo_dir" {
     type = "string"
     description = "Location to save repo on Local System"
 }
+variable "module" {
+    default = false
+    type = "string"
+    description = "Is this a Terraform Module? if so, set to true"
+}
